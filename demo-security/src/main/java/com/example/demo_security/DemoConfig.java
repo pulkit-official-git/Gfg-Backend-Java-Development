9de85e52fee26c0343dd67d8db223715cfe327e4
scalargitid <pulkit.sharma.2406@gmail.com>
2025-05-23 12:01:55 +0530

    adding db security and signup

---
+++ demo-security/src/main/java/com/example/demo_security/DemoConfig.java
@@ -0,0 +1,69 @@
+package com.example.demo_security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoConfig {
+
+    @Autowired
+    DemoUserDetailsService demoUserDetailsService;
+
+    @Autowired
+    PasswordEncoderConfig passwordEncoderConfig;
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authBuilder.userDetailsService(demoUserDetailsService).passwordEncoder(this.passwordEncoderConfig.passwordEncoder());
+        return authBuilder.build();
+    }
+
+
+//    @Bean
+//    public InMemoryUserDetailsManager cpnfigureAuthentication() {
+//
+//        UserDetails user1 = User.builder()
+//                .username("admin")
+//                .password("$2a$12$D.8.OH0rsILUU4PzObHd1e/SLR12xFURorzwFZ4QDxKBbO0EuP.ba")
+//                .authorities("ADMIN")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("user")
+//                .password("$2a$12$UMBy9vmAqBgXRmnVWrY65.trrrdAwObUy1X/2gb1o2Ymkv6ULelcS")
+//                .authorities("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/signup").permitAll()
+                        .requestMatchers("/user/**").hasAuthority("USER")
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ demo-security/src/main/java/com/example/demo_security/DemoUser.java
@@ -0,0 +1,43 @@
+package com.example.demo_security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DemoUser implements UserDetails {
+
+    public static final String DELIMITER = "::";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String authorities;
+
+    public Collection<GrantedAuthority> getAuthorities() {
+        String [] authorities = this.authorities.split(DELIMITER);
+        return Arrays.stream(authorities)
+                .map(x->new SimpleGrantedAuthority(x))
+                .collect(Collectors.toList());
+    }
+
+
+}
